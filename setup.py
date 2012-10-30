dacc4dea49deb15ddcb1336cbb3e14967a49c312
Alexey Kostyuk <unitoff@gmail.com>
2012-10-30 10:21:25 +0400

    Added post-install. Added virtualenv hack

--- setup.py
+++ setup.py
@@ -1,14 +1,31 @@
 import os
+import subprocess
 from distutils.core import setup
+from distutils.command.install_data import install_data
 
 # Hack for virtualenv
 venv = os.environ.get('VIRTUAL_ENV')
 if venv:
-    usr_prefix = 'usr/'
+    usr_prefix = os.path.join(venv, 'usr')
 else:
     usr_prefix = '/usr/'
 
+
+class post_install(install_data):
+    def run(self):
+        # Call parent
+        install_data.run(self)
+        # Execute commands
+        if os.path.exists('%sshare/icons/ubuntu-mono-dark/.icon-theme.cache' %
+                            usr_prefix):
+            print "Updating icons cache"
+            subprocess.call(['/usr/bin/gtk-update-icon-cache',
+                            '-f', '-t', '%sshare/icons/ubuntu-mono-dark' %
+                            usr_prefix])
+
+
 setup(name='vbox-indicator',
+    cmdclass={"install_data": post_install},
     version = '0.1',
     description = 'Simple application indicator for VirtualBox',
     author = 'Alexey Kostyuk',
